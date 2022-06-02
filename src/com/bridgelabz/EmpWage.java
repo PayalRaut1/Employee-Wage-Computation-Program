1406e3ce2eac4c24f8dcf3d934f0161e57370dbc
PayalRaut1 <106006513+PayalRaut1@users.noreply.github.com>
2022-06-02 23:11:10 +0530

    Update EmpWage.java

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,7 +4,6 @@ public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee wage Program");
         int IS_FULL_TIME = 1;
-        // Computation
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME)
             System.out.println("Employee is Present");
